77f4de4e00115da69fe93c83362931afecfc3f3a
Zane Wolfe <85024901+Zane-Wolfe@users.noreply.github.com>
2025-03-27 16:50:50 -0400

    Added GameData Scriptable Object

---
+++ Assets/Scripts/Scriptable Objects/GameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData", order = 1)]
+public class GameData : ScriptableObject
+{
+    public float playerMoney = 0;
+
+
+}
