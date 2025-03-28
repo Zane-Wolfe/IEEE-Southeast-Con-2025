7c0268691a1d26684728d73748887044be934dcb
MisterCheese27 <themrcheese27@gmail.com>
2025-03-28 13:20:12 -0400

    Update on Upgrade System

--- Assets/Scripts/UpgradeStation/UpgradeStation.cs
+++ Assets/Scripts/UpgradeStation/UpgradeStation.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UpgradeStation : MonoBehaviour, IInteractable
 {
+    [SerializeField] GameData gameData;
     [SerializeField] GameObject upgradesMenu;
+    [SerializeField] GameObject moneyTextObject;
+
+    private TMPro.TextMeshProUGUI moneyText;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(upgradesMenu.ToString());
+        // upgradesMenu.SetActive(false);
+        moneyText = moneyTextObject.GetComponent<TMPro.TextMeshProUGUI>();
+        moneyText.text = "Money:\n$" + gameData.playerMoney.ToString();
     }
 
     // Update is called once per frame
