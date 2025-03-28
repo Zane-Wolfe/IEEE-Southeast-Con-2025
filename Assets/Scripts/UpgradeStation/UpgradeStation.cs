ec199c3405658a1423e170d1ef2f94800e3faf13
MisterCheese27 <themrcheese27@gmail.com>
2025-03-28 00:00:30 -0400

    Worked on Upgrade System

--- Assets/Scripts/UpgradeStation/UpgradeStation.cs
+++ Assets/Scripts/UpgradeStation/UpgradeStation.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeStation : MonoBehaviour, IInteractable
 {
-    
+    [SerializeField] GameObject upgradesMenu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(upgradesMenu.ToString());
     }
 
     // Update is called once per frame
@@ -21,5 +22,6 @@ public class UpgradeStation : MonoBehaviour, IInteractable
     public void Interact()
     {
         Debug.Log("Entered Upgrade Station");
+        upgradesMenu.SetActive(true);
     }
 }
---
+++ Assets/Scripts/UpgradeStation/UpgradeTextUpdater.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeTextUpdater : MonoBehaviour
+{
+    [SerializeField] GameData upgradesMenu;
+    [SerializeField] GameObject levelText;
+    [SerializeField] GameObject buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
