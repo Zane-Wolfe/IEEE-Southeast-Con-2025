c0bcbf17da51ea602e5978896617994ff53009ea
MisterCheese27 <themrcheese27@gmail.com>
2025-03-27 21:52:34 -0400

    Upgrade System stuff

--- Assets/Scripts/Scriptable Objects/GameData.cs
+++ Assets/Scripts/Scriptable Objects/GameData.cs
@@ -7,5 +7,20 @@ public class GameData : ScriptableObject
 {
     public float playerMoney = 0;
 
+    // Upgrades System
+    // Constants
+    public int walkingSpeed = -1;
+    public int carSpeed = -1;
+    public int pickQuality = -1;
+    public int chiselQuality = -1;
+    public int walkSpeedUpgradeCost = -1;
+    public int carSpeedUpgradeCost = -1;
+    public int pickUpgradeCost = -1;
+    public int chiselUpgradeCost = -1;
 
+    // Levels
+    public int walkSpeedLevel = 1;
+    public int carSpeedLevel = 1;
+    public int pickLevel = 1;
+    public int chiselLevel = 1;
 }
