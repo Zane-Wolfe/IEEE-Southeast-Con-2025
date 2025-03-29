3f18026d5ead2b65dfacfbe7d579a79637736655
MisterCheese27 <themrcheese27@gmail.com>
2025-03-29 04:16:33 -0400

    Created sculpture selection

--- Assets/Scripts/NPCs/NPCBehavior.cs
+++ Assets/Scripts/NPCs/NPCBehavior.cs
@@ -25,24 +25,20 @@ public class NPCBehavior : MonoBehaviour
         maxX = walkingArea.position.x + xRadius;
         minZ = walkingArea.position.z - zRadius;
         maxZ = walkingArea.position.z + zRadius;
-        Debug.Log(gameObject.transform.position);
         StartCoroutine(WaitAndMove());
-        // InvokeRepeating("MoveNPC", 1, 3);
-        // Debug.Log("");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // npcTransform.LookAt(new Vector3(0, 0, 0));
-        // gameObject.transform.LookAt(new Vector3(minX, gameObject.transform.position.y, maxZ));
+        
     }
 
     IEnumerator WaitAndMove() {
         yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
         float startTime = Time.time;
         Vector3 initialPos = gameObject.transform.position;
-        Vector3 targetPos = new Vector3(Random.Range(minX, maxX), initialPos.y, Random.Range(minZ, maxZ));
+        Vector3 targetPos = new(Random.Range(minX, maxX), initialPos.y, Random.Range(minZ, maxZ));
 
         gameObject.transform.LookAt(targetPos);
         while(Time.time - startTime <= Random.Range(minWalkTime, maxWalkTime)) {
@@ -51,16 +47,4 @@ public class NPCBehavior : MonoBehaviour
         }
         StartCoroutine(WaitAndMove());
     }
-
-    // void MoveNPC() {
-    //     Debug.Log("Start");
-    //     float startTime = Time.time;
-    //     Vector3 initialPos = gameObject.transform.position;
-    //     Vector3 targetPos = new Vector3(Random.Range(minX, maxX), initialPos.y, Random.Range(minZ, maxZ));
-    //     while(Time.time - startTime <= 1) {
-    //         gameObject.transform.position = Vector3.Lerp(initialPos, targetPos, Time.time - startTime);
-    //         // yield return 1;
-    //     }
-    //     Debug.Log("Finish");
-    // }
 }
